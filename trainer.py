dab0be8157edbdca735396273b90a689c69b94a1
palam <pawarit.a@deeple.ai>
2023-01-30 02:04:23 +0700

    train 1.4b on deepspeed (2)

--- trainer.py
+++ trainer.py
@@ -1,4 +1,4 @@
-from transformers import Trainer, TrainingArguments, AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoTokenizer, default_data_collator, set_seed
+from transformers import Trainer, TrainingArguments, AutoModelForCausalLM, AutoConfig, AutoModelForSeq2SeqLM, AutoTokenizer, default_data_collator, set_seed
 from dataset.dataset import EOSSplitTextDataset
 from omegaconf import OmegaConf
 import os
@@ -9,20 +9,29 @@ set_seed(365)
 
 
 class CustomTrainer(Trainer):
-    def __init__(self, config, result_dir: str, **kwargs):
+    def __init__(self, config, result_dir: str):
         self.config = config
         self.is_enc_dec = 't5' in self.config.model_name
+        model_config = AutoConfig.from_pretrained(self.config.model_name)
+        model_config.gradient_checkpointing = True
+        model_config.use_cache = False
+
         if self.is_enc_dec:
             self.model = AutoModelForSeq2SeqLM.from_pretrained(
-                self.config.model_name)
+                self.config.model_name, config=model_config)
         else:
             self.model = AutoModelForCausalLM.from_pretrained(
-                self.config.model_name)
+                self.config.model_name, config=model_config)
+        if is_main_process():
+            # print(self.model)
+            total_train_params = sum(p.numel()
+                                     for p in self.model.parameters() if p.requires_grad)
+            print(f'Total train param: {total_train_params:,}')
         self._set_tokenizer()
         training_args = TrainingArguments(
-            do_train=True, do_eval=True, evaluation_strategy='steps', output_dir=result_dir, dataloader_num_workers=8, **self.config.training_args)
+            do_train=True, do_eval=True, evaluation_strategy='steps', output_dir=result_dir, dataloader_num_workers=0, **self.config.training_args)
         super().__init__(self.model, training_args, default_data_collator, self._get_train_dataset(),
-                         self._get_eval_dataset(), self.tokenizer, **kwargs)
+                         self._get_eval_dataset(), self.tokenizer, **config.get('trainer_args', {}))
 
     def _set_tokenizer(self):
         self.tokenizer = AutoTokenizer.from_pretrained(self.config.model_name)
