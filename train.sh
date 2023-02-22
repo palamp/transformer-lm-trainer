8882e5ec8900e5f2ef1d0f8c3d4bbb569f4c482a
palam <pawarit.a@deeple.ai>
2023-02-22 11:59:49 +0700

    add mt5 config

--- train.sh
+++ train.sh
@@ -3,4 +3,5 @@
 # deepspeed --num_gpus=2 trainer.py --config config/config_encdec.yaml
 # deepspeed --num_gpus=2 trainer.py --config config/config_clm_story.yaml
 # python trainer.py
-deepspeed --include="localhost:0" trainer.py --config config/config_paraphase.yaml
+# deepspeed --include="localhost:0" trainer.py --config config/config_paraphase.yaml
+CUDA_VISIBLE_DEVICES=1 python trainer.py --config config/config_paraphase_mt5.yaml
