f58840f1b4edfa7945a296865a0a225af2f4222b
palam <pawarit.a@deeple.ai>
2023-05-07 19:19:53 +0700

    feat: add 8bit adam optimizer

--- trainer.py
+++ trainer.py
@@ -1,5 +1,8 @@
-import os
+from pathlib import Path
 
+import bitsandbytes as bnb
+from omegaconf import DictConfig
+from torch import nn
 from transformers import (
     AutoConfig,
     AutoModelForCausalLM,
@@ -10,6 +13,7 @@ from transformers import (
     default_data_collator,
     set_seed,
 )
+from transformers.trainer_pt_utils import get_parameter_names
 
 from callback import GenerationCallback
 from dataset import CloneDataset, EOSSplitTextDataset, JSONDataset
@@ -20,19 +24,27 @@ set_seed(42)
 
 
 class CustomTrainer(Trainer):
-    def __init__(self, config):
+    def __init__(self, config: DictConfig, result_dir: Path):
         self.config = config
 
         model_config = AutoConfig.from_pretrained(self.config.model.name)
         model_config.gradient_checkpointing = True
         model_config.use_cache = False
 
-        if self.config.model.type == "enc_dec":
+        if self.config.model.arch == "enc_dec":
             model = AutoModelForSeq2SeqLM.from_pretrained(self.config.model.name, config=model_config)
-        elif self.config.model.type == "dec_only":
-            model = AutoModelForCausalLM.from_pretrained(self.config.model.name, config=model_config)
+        elif self.config.model.arch == "dec_only":
+            model = AutoModelForCausalLM.from_pretrained(self.config.model.name, config=model_config).to(
+                "cuda"
+            )
 
-        tokenizer = AutoTokenizer.from_pretrained(self.config.model.get("tokenizer", self.config.model.name))
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.config.model.get("tokenizer", self.config.model.name)
+        )
+        self.tokenizer.add_special_tokens(
+            {"additional_special_tokens": ["<TH_CLS>", "<EN_CLS>"], "cls_token": "<PP_CLS>"}
+        )
+        model.resize_token_embeddings(len(self.tokenizer))
 
         self.train_dataset = self._get_train_dataset()
         self.eval_dataset = self._get_eval_dataset()
@@ -42,8 +54,8 @@ class CustomTrainer(Trainer):
             do_eval=True,
             evaluation_strategy="steps",
             output_dir=result_dir,
-            dataloader_num_workers=0,
-            learning_rate=self.config.lr,
+            report_to="wandb",
+            run_name=f"{self.config.model.name}-{result_dir.name}",
             **self.config.training_args,
         )
 
@@ -57,24 +69,46 @@ class CustomTrainer(Trainer):
             default_data_collator,
             self.train_dataset,
             self.eval_dataset,
-            tokenizer,
+            self.tokenizer,
             callbacks=[GenerationCallback(self.config, result_dir)],
+            optimizers=(self._get_adam_8bit(model, training_args), None),
             **config.get("trainer_args", {}),
         )
 
+    def _get_adam_8bit(self, model, training_args):
+        decay_parameters = get_parameter_names(model, [nn.LayerNorm])
+        decay_parameters = [name for name in decay_parameters if "bias" not in name]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in model.named_parameters() if n in decay_parameters],
+                "weight_decay": training_args.weight_decay,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if n not in decay_parameters],
+                "weight_decay": 0.0,
+            },
+        ]
+
+        return bnb.optim.Adam8bit(
+            optimizer_grouped_parameters,
+            betas=(training_args.adam_beta1, training_args.adam_beta2),
+            eps=training_args.adam_epsilon,
+            lr=training_args.learning_rate,
+        )
+
     def _get_dataset(self, path: str):
-        dataset_type = self.config.get("dataset_type", "clm")
-        if dataset_type == "clm":
+        if self.config.dataset_type == "clm":
             return EOSSplitTextDataset(
                 path,
-                tokenizer_name=self.config.get("tokenizer_name", self.config.model_name),
-                arch="clm",
+                tokenizer=self.tokenizer,
+                arch=self.config.model.arch,
                 **self.config.data.get("config", {}),
             )
-        elif dataset_type == "paraphase_json":
+        elif self.config.dataset_type == "paraphase_json":
             return JSONDataset(
                 path,
-                tokenizer_name=self.config.get("tokenizer_name", self.config.model_name),
+                tokenizer=self.tokenizer,
+                arch=self.config.model.arch,
                 **self.config.data.get("config", {}),
             )
         else:
@@ -113,11 +147,11 @@ if __name__ == "__main__":
 
     print(f"Config {conf_file}")
     conf = OmegaConf.load(conf_file)
-    result_dir = get_result_dir()
+    result_dir = Path(get_result_dir())
     if is_main_process(args.local_rank):
         print("Creating: ", result_dir)
-        os.makedirs(result_dir, exist_ok=False)
-        shutil.copy(conf_file, f"{result_dir}/config.yaml")
+        result_dir.mkdir()
+        shutil.copy(conf_file, result_dir / "config.yaml")
     trainer = CustomTrainer(conf, result_dir)
     train_result = trainer.train()
     trainer.save_model()
